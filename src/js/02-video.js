a5fe54b4f6a30020744d77d8fec328c67a86709d
NazarkoKril <122899742+NazarkoKril@users.noreply.github.com>
2023-07-01 18:08:02 +0300

    /

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const PLAYER_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlayTime, 1000));
+
+function onPlayTime(data) {
+  const currentTime = data.seconds;
+
+  localStorage.setItem(PLAYER_KEY, currentTime);
+}
+
+player.setCurrentTime(localStorage.getItem(PLAYER_KEY) || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
 
+
+import throttle from 'lodash.throttle';
+
+
+ const formEl = document.querySelector('.feedback-form');
+ const emailEl = document.querySelector('.feedback-form input');
+ const messageEl = document.querySelector('.feedback-form textarea');
+
+
+const INPUT_KEY = 'feedback-form-state';
+const localString = localStorage.getItem(INPUT_KEY);
+const localObject = JSON.parse(localString);
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+if (localObject) {
+  emailEl.value = localObject.email;
+  messageEl.value = localObject.message;
+}
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput() {
+  formData = {
+    email: emailEl.value,
+    message: messageEl.value,
+  };
+
+  localStorage.setItem(INPUT_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  formData = {
+    email: emailEl.value,
+    message: messageEl.value,
+  };
+
+  if (emailEl.value === '' || messageEl.value === '') {
+    return alert('Всі поля потрібно заповнити!');
+  }
+
+  console.log(formData);
+  localStorage.removeItem(INPUT_KEY);
+  formEl.reset();
+};
